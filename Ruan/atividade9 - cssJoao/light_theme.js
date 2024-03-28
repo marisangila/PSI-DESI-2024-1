f0423261bc938803bb770e813c7e5fb1e801d7de
ruansilva123 <ruancplay59@gmail.com>
2024-03-28 13:39:08 -0300

    attEight

--- Ruan/atividade9 - cssJoao/light_theme.js
+++ Ruan/atividade9 - cssJoao/light_theme.js
@@ -5,25 +5,24 @@ function alterarTema() {
     if (option == "Modo escuro") {
         const fundo = document.getElementById("background-body");
         fundo.style.backgroundColor = "#000000";
+        
+        const div = document.getElementById("mudaTema");
+        div.innerHTML = "Modo claro";
 
         const texto = document.getElementsByClassName("texto");
         for (var i = 0; 0 < texto.length; i++) {
             texto.item(i).style.color = "#ffffff";
         }
-
-        const div = document.getElementById("mudaTema");
-        div.innerHTML = "Modo claro";
     } else {
         const fundo = document.getElementById("background-body");
         fundo.style.backgroundColor = "#ffffff";
+        
+        const div = document.getElementById("mudaTema");
+        div.innerHTML = "Modo escuro";
 
         const texto = document.getElementsByClassName("texto");
         for (var i = 0; 0 < texto.length; i++) {
             texto.item(i).style.color = "#000000";
         }
-
-        const div = document.getElementById("mudaTema");
-        div.innerHTML = "Modo escuro";
     }
 }
-// não está alterando o valor dentro da div
