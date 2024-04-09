72ea00f7e709f47ee8a8a326784bb4c529e594c2
ErosBenkert <eros_benkert@estudante.sesisenai.org.br>
2024-04-09 16:30:23 -0300

    a

---
+++ Eros/db_atividade10/insert.sql
@@ -0,0 +1,22 @@
+-- 1 tb_fornecedores
+-- 2 tb_compras
+-- 3 tb_ingredientes
+-- 4 tb_cardapios
+-- 5 tb_receitas
+-- 6 tb_categorias_pasteis
+-- 7 tb_categorias_pizzas
+-- 8 tb_pasteis
+-- 9 tb_pizzas
+-- 10 tb_pedidos
+-- 11 tb_entregadores
+-- 12 tb_entregas
+
+
+INSERT INTO tb_
+VALUES
+(col_),
+(col_),
+(col_),
+(col_),
+(col_)
+;
