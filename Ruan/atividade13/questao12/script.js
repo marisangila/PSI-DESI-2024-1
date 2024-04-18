34a4ba26c1dde36a54002ec797f96fad0eebfea1
ruansilva123 <ruancplay59@gmail.com>
2024-04-18 14:26:19 -0300

    activityCompleted

--- Ruan/atividade13/questao12/script.js
+++ Ruan/atividade13/questao12/script.js
@@ -1,6 +1,28 @@
-var vidaMario
+var vidaMario 
 
 function iniciarVidaMario(){
     vidaMario = 5
-    document.getElementById("vidaMario").innerHTML = `${vidaMario}`
+    document.getElementById("vidaMario").innerHTML = vidaMario
+}
+
+function alterarVidaMario(alteraVidaMario){
+
+    if (vidaMario == 0 && alteraVidaMario){
+        alert('O Mário já está morto!!!')
+    }else if (vidaMario == 5 && !alteraVidaMario){
+        alert('A vida do Mário está cheia!!!')
+    }else{
+        if (alteraVidaMario){
+            vidaMario -= 1
+            document.getElementById("vidaMario").innerHTML = vidaMario
+        }else{
+            vidaMario += 1
+            document.getElementById("vidaMario").innerHTML = vidaMario
+        }
+        if (vidaMario == 1 && alteraVidaMario){
+            alert('O Mário morrerá no próximo ataque!!!')
+        }else if(vidaMario == 1 && !alteraVidaMario){
+            alert('O Mário Ressuscitou!!!')
+        }
+    }
 }
