0a26f991457ebd179ae9c4e60140ddebe5774676
GustavoPaluski <guorkut@gmail.com>
2024-04-18 17:22:00 -0300

    atividade 13

--- Gustavo/atividade_13/ex08/scripts.js
+++ Gustavo/atividade_13/ex08/scripts.js
@@ -1,11 +1,15 @@
 printar = ""
-var anterior = 0
+var anterior = 1
 var atual = 0
+var nova = 0
+var soma = 0
 
-for(var nova=0; nova<300; nova++){
+while(nova < 50000){
     nova = atual + anterior
     anterior = atual
     atual = nova
-    console.log(nova + " ")
+    if(nova % 2 == 0){soma += nova}
 }
+
+console.log(soma)
 // TA ERRADO OS CALCULOS 
---
+++ Gustavo/atividade_13/ex12/scripts.js
@@ -0,0 +1,4 @@
+function DecideVida(dano, saude){
+    if(dano > saude){return 1}
+    else{return 0}
+}
