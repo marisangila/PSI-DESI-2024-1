7bc5fe5166f36942f8d056727093a73e7a262060
Brunno Oliveira <brunnopedro.oliveira@gmail.com>
2024-04-18 16:13:25 -0300

    update

--- Brunno/atividade13/atividade9/script.js
+++ Brunno/atividade13/atividade9/script.js
@@ -1,1 +1,5 @@
-alert("Alerta")
+base = Number(prompt("Informe a base: "))
+expoente = Number(prompt("Informe o expoente: "))
+
+resultado = base**expoente
+alert(`O valor do cálculo: ${base} elevado a ${expoente} é igual a ${resultado}`)
