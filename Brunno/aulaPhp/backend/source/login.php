2b3ffbbba3cc5dc120b2c1bd229b7b1e7e6c5b43
Brunno Oliveira <brunnopedro.oliveira@gmail.com>
2024-05-21 16:50:13 -0300

    add

---
+++ Brunno/aulaPhp/backend/source/login.php
@@ -0,0 +1,27 @@
+<?php
+    include_once("conexao.php");
+
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    $sql = $conn->prepare("SELECT pk_usuario, email_usuario, senha_usuario, adm_role FROM usuario WHERE email_usuario = ?");
+
+    $sql->execute([$email]);
+
+    if($sql->rowCount() == 1){
+        $usuario = $sql->fetch();
+        if ($usuario['senha_usuario'] == MD5($senha)){
+            session_start();
+            $_SESSION["adm_role"] = $usuario['adm_role'];
+            $_SESSION['loggedin'] = true;
+            header("location:../index.html");
+        }else{
+            echo("E-mail ou senha inválidos!");
+        }
+    }else{
+        echo("E-mail ou senha inválidos!");
+    }
+
+    unset($sql);
+    unset($conn);
+?>
