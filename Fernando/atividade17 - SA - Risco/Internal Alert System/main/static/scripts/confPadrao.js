dd38fdde535149c3b938d15fdf30ef042d54264f
FernandoBackup <praquequeromeuemail@gmail.com>
2024-07-15 16:20:08 -0300

    top

--- Fernando/atividade17 - SA - Risco/Internal Alert System/main/static/scripts/confPadrao.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    // Verifica se a tecla pressionada é a tecla "Enter"
-    if (event.keyCode === 13) {
-        // Cancela o comportamento padrão da tecla "Enter"
-        event.preventDefault();
-    }
-});
